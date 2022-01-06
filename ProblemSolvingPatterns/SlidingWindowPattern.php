5cfabb7fbb31ef1c92b1d085b97cdc35feb794e6
Mohamed Alaa <m.3laa.95@gmail.com>
2022-01-06 22:52:33 +0200

    ading SlidingWindowPattern

---
+++ ProblemSolvingPatterns/SlidingWindowPattern.php
@@ -0,0 +1,36 @@
+<?php
+
+echo "SlidingWindowPattern";
+
+// // example 1
+// $numbers = [1, 2, 5, 2, 8, 1, 5];
+// $num = 2;
+
+// // example 2
+// $numbers = [1,2,5,2,8,1,5];
+// $num = 4;
+
+// example 3
+$numbers = [4, 2, 1, 6];
+$num = 1;
+
+// // example 4
+// $numbers = [4,2,1,6,2];
+// $num = 4;
+function maxSubArraySum($numbers, $num)
+{
+    if ($num == 1) {
+        return max($numbers);
+    }
+
+    for ($i = 0; $i < count($numbers) - 1; $i++) {
+        for ($j = $i; $j <= ($i + $num) - 1; $j++) {
+            $sum[$i] = isset($sum[$i]) ? $sum[$i] + $numbers[$j] : $numbers[$i];
+        }
+    }
+
+    return max($sum);
+}
+
+
+var_dump(maxSubArraySum($numbers, $num));
