234bafdc5c4ee3b5885f7167cca724f83847809b
Mohamed Alaa <m.3laa.95@gmail.com>
2022-01-07 21:29:17 +0200

    maxSubarraySum

---
+++ Task/maxSubarraySum.php
@@ -0,0 +1,28 @@
+<?php
+
+// Sliding Window - maxSubarraySum
+// Given an array of integers and a number, write a function called  maxSubarraySum, which finds the maximum sum of a subarray with the length of the number passed to the function. Note that a subarray must consist of consecutive elements from the original array. In the first example below, [100, 200, 300] is a subarray of the original array, but [100, 300] is not.
+
+$numbers = [1, 4, 2, 10, 23, 3, 1, 0, 20];
+$count = 4;
+function maxSubarraySum($numbers, $num)
+{
+    $max = 0;
+    for ($i = 0; $i < $num; $i++) {
+        $max += $numbers[$i];
+    }
+
+    $sum = $max;
+    $total = 0;
+    for ($i = $num; $i < count($numbers); $i++) {
+        var_dump($i);
+        $sum += $numbers[$i] - $numbers[$i - $num];
+        if ($sum  > $total) {
+            $total = $sum;
+        }
+    }
+
+    return $total;
+}
+
+var_dump(maxSubarraySum($numbers, $count));
