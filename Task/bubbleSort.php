5027169215af01d3b770c557f5de9e38452c0d75
Mohamed Alaa <m.3laa.95@gmail.com>
2022-01-20 06:49:07 +0200

    bubbleSort

--- Task/bubbleSort.php
+++ Task/bubbleSort.php
@@ -8,20 +8,23 @@
  */
 function bubbleSort($nums)
 {
-    for ($i = 0; $i < count($nums); $i++) {
-        for ($j = 0; $j < count($nums) - 1; $j++) {
-            $current  = $nums[$j];
-            $next = $nums[$j + 1];
-            if ($current > $next) {
-                $temp = $current;
-                $nums[$j] = $next;
+    for ($i = 0; $i < count($nums) - 1; $i++) {
+        $flag = true;
+        for ($j = 0; $j < count($nums) - $i - 1; $j++) {
+            if ($nums[$j] > $nums[$j + 1]) {
+                $temp = $nums[$j];
+                $nums[$j] = $nums[$j + 1];
                 $nums[$j + 1] = $temp;
+                $flag = false;
             }
+        }
+        if ($flag == true) {
+            break;
         }
     }
     return $nums;
 }
 
-var_dump(bubbleSort([1, 3, 9, 2, 10]));
+var_dump(bubbleSort([1,  2, 3, 4, 5, 6]));
 var_dump(bubbleSort([10, 8, 7, 6, 5, 4, 3, 2, 1]));
 var_dump(bubbleSort([34, 22, 10, 19, 17]));
