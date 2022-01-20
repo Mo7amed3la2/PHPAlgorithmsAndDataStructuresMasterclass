aaba61c1bb30afa21b66cd15823b883c4601d6da
Mohamed Alaa <m.3laa.95@gmail.com>
2022-01-20 08:35:56 +0200

    enhancement code

--- Task/bubbleSort.php
+++ Task/bubbleSort.php
@@ -8,9 +8,11 @@
  */
 function bubbleSort($nums)
 {
+    $c = 0;
     for ($i = 0; $i < count($nums) - 1; $i++) {
         $flag = true;
         for ($j = 0; $j < count($nums) - $i - 1; $j++) {
+            $c++;
             if ($nums[$j] > $nums[$j + 1]) {
                 $temp = $nums[$j];
                 $nums[$j] = $nums[$j + 1];
@@ -22,9 +24,9 @@ function bubbleSort($nums)
             break;
         }
     }
-    return $nums;
+    var_dump($nums,$c);
 }
 
-var_dump(bubbleSort([1,  2, 3, 4, 5, 6]));
-var_dump(bubbleSort([10, 8, 7, 6, 5, 4, 3, 2, 1]));
+var_dump(bubbleSort([10, 8, 7, 6, 5, 4, 3, 1,2]));
+var_dump(bubbleSort([1, 3, 9, 2, 10]));
 var_dump(bubbleSort([34, 22, 10, 19, 17]));
--- Task/selectionSort.php
+++ Task/selectionSort.php
@@ -8,20 +8,22 @@
  */
 function selectionSort($nums)
 {
+    $c = 0;
     for ($i = 0; $i < count($nums); $i++) {
-        $min  = $nums[$i];
+        $min = $i;
         for ($j = $i + 1; $j < count($nums); $j++) {
-            $next = $nums[$j];
-            if ($min > $next) {
-                $nums[$i] = $next;
-                $nums[$j] = $min;
-                $min = $next;
+            $c++;
+            if ($nums[$j] < $nums[$min]) {
+                $min = $j;
             }
         }
+        $temp = $nums[$i];
+        $nums[$i] = $nums[$min];
+        $nums[$min] = $temp;
     }
-    return $nums;
+    var_dump($nums, $c);
 }
 
+var_dump(selectionSort([10, 8, 7, 6, 5, 4, 3, 1, 2]));
 var_dump(selectionSort([1, 3, 9, 2, 10]));
-var_dump(selectionSort([10, 8, 7, 6, 5, 4, 3, 2, 1]));
 var_dump(selectionSort([34, 22, 10, 19, 17]));
